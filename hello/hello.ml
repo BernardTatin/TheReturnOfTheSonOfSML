1444f477fd5f6670f3210d2e17675957acab7bf3
Bernard Tatin <bernard.tatin@outlook.fr>
2018-04-04 00:46:15 +0200

    clean code

--- hello/hello.ml
+++ hello/hello.ml
@@ -6,6 +6,9 @@
  *
  * To try it:
  *    polyc -o hello hello.ml && ./hello *
+ * or, better:
+ *    make test 
+ *
  *)
 
 
@@ -16,12 +19,12 @@ fun main() =
       fun processArgs []      =
          (print "Hello, brave new world!\n";
           OS.Process.exit OS.Process.success)
-       |  processArgs(x::tail) = (
-            print ("Hello " ^ x ^ "\n"); 
-            processArgs tail
-         )
+
+       |  processArgs(x::tail) = 
+            (print ("Hello " ^ x ^ "\n"); 
+            processArgs tail)
    in 
-      ( stdArgs (CommandLine.arguments());
+      (stdArgs (CommandLine.arguments());
         processArgs (CommandLine.arguments()))
    end
 
--- hello/stdArgs.ml
+++ hello/stdArgs.ml
@@ -8,14 +8,17 @@
 
 fun stdArgs [] = 
    print("\n")
+
 | stdArgs("--help"::tail) =
    (print "hello [--help|--version]\n";
     print " --help: this text\n";
     print " --version: print version and exit\n";
     OS.Process.exit OS.Process.success)
+
 | stdArgs("--version"::tail) =
    (print "Hello version 0.0.0\n";
     OS.Process.exit OS.Process.success)
+
 | stdArgs(_::tail) =
    stdArgs tail     
 
