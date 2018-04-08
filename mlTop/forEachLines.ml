0421fd47ff3b7eac72e0117b3a1616cd46832cde
Bernard Tatin <bernard.tatin@outlook.fr>
2018-04-08 12:07:59 +0200

    Nice comments

--- mlTop/forEachLines.ml
+++ mlTop/forEachLines.ml
@@ -3,9 +3,24 @@
  * Copyright (C) 2018 bernard <bernard@bernard-LIFEBOOK-E782>
  *
  * Distributed under terms of the MIT license.
+ *
+ * this module contains the function forEachLines and all
+ * the associated stuff if any
  *)
 
- fun forEachLines (fileName : string, onLine : string -> unit) : unit = let
+(*
+ * function forEachLines
+ * parameters
+ *    fileName : the name of the file
+ *    onLine : the function applied on each lines when they are loaded
+ *        parameters
+ *          line: the line to process
+ *        return
+ *          unit
+ * return
+ *    unit
+ *)
+fun forEachLines (fileName : string, onLine : string -> unit) : unit = let
    val inputHandle = TextIO.openIn fileName
 
    fun loop inputHandle =
--- mlTop/tools.ml
+++ mlTop/tools.ml
@@ -3,6 +3,17 @@
  * Copyright (C) 2018 bernard <bernard@bernard-LIFEBOOK-E782>
  *
  * Distributed under terms of the MIT license.
+ *
+ * This module contains various functions which are useful
+ * in mlTop module.
  *)
 
- fun isSemiColon c = c = #":"
+(*
+ * function isSemiColon
+ * parameters
+ *    c, the char to test
+ * return
+ *    true if c is a semi-colon (:)
+ *    false otherwise
+ *)
+fun isSemiColon c = c = #":"
