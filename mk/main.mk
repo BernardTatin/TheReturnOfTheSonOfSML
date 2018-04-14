39205a61ce473834e20c83a39b96b4f2128b1a4f
Bernard Tatin <bernard.tatin@outlook.fr>
2018-04-14 07:47:49 +0200

    Compile and test for PolyML and MLTon

--- mk/main.mk
+++ mk/main.mk
@@ -12,15 +12,37 @@ EXE = $(PROJECT).exe
 ARCHIVE = $(PROJECT).tar.gz
 
 POLYML = polyc
+POLDIR = polyml
+POLEXE = $(POLDIR)/$(EXE)
 
-all: $(EXE)
+MLTON = mlton
+MLTOPT = -verbose 1  -cc-opt -O2
+MLTDIR = mltml
+MLTEXE = $(MLTDIR)/$(EXE)
+MLTMLB = $(PROJECT).mlb
 
-$(EXE): $(MAIN) $(DEPS)
-	$(POLYML) -o $(EXE) $(MAIN)
+all: polexe mltexe
 
+polexe: poldir $(POLEXE)
+poldir: $(POLDIR)
+$(POLDIR):
+	mkdir -p $@
+.PHONY: polexe poldir
+
+$(POLEXE): polyMain.sml $(MAIN) $(DEPS)
+	$(POLYML) -o $@ $<
+
+mltexe: mltdir $(MLTDIR)/$(EXE)
+mltdir: $(MLTDIR)
+$(MLTDIR):
+	mkdir -p $@
+.PHONY: mltexe mltdir
+
+$(MLTEXE): $(MLTMLB) $(MAIN) $(DEPS)
+	$(MLTON) -output $@ $(MLTOPT) $<
 
 smallclean:
-	rm -fv $(EXE)
+	rm -fv $(POLEXE) $(MLTEXE)
 
 clean: smallclean
 	rm -fv ../$(ARCHIVE)
@@ -28,14 +50,18 @@ clean: smallclean
 archive: smallclean
 	cd ..; tar czf $(ARCHIVE) $(PROJECT)
 
-install: $(EXE)
+install: $(POLYEXE)
 	mkdir -p $(PREFIX)/bin
-	cp $(EXE) $(PREFIX)/bin
+	cp $< $(PREFIX)/bin
 
 uninstall:
 	rm -fv $(PREFIX)/bin/$(EXE)
 
-test: $(EXE)
-	./$(EXE) $(TEST_ARGS)
+testpoly: polexe
+	$(POLEXE) $(TEST_ARGS)
+
+testmlt: mltexe
+	$(MLTEXE) $(TEST_ARGS)
 
 .PHONY: all smallclean clean archive install
+.PHONY: testpoly testmlt
